a3d05dd5e3a9c1c0d77b3ee55b5906d5a70d7bc7
Daniel Candeira <danielbarr16@gmail.com>
2022-11-15 09:05:54 -0300

    feat: try to fix sass build

--- .eleventy.js
+++ .eleventy.js
@@ -31,6 +31,7 @@ module.exports = config => {
 
   // Pass through fonts and images
   config.addPassthroughCopy('./src/fonts');
+  config.addPassthroughCopy('./src/css');
   config.addPassthroughCopy('./src/images');
   config.addPassthroughCopy('./src/favicon.ico');
 
